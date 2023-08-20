72f239ab36f4e796fce99248ee05e6421f45bc4f
Juan Miguel <caber_t@hotmail.com>
2023-08-20 21:44:30 +0200

    added function dark mode, renamed the folder resource to assets

--- functions.js
+++ functions.js
@@ -1,3 +1,19 @@
 document.getElementById("printButton").addEventListener("click", function() {
     window.print();
 });
+const toggleButton = document.getElementById("toggleTheme");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme === "dark") {
+    document.body.classList.add("dark-theme");
+}
+
+toggleButton.addEventListener("click", function() {
+    document.body.classList.toggle("dark-theme");
+    let theme = "light";
+    if (document.body.classList.contains("dark-theme")) {
+        theme = "dark";
+    }
+    localStorage.setItem("theme", theme);
+});
+
